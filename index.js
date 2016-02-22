49e507fc8e1ade0bc64170bbc82cfd1fe0db64ba
raelcun <dt.raelcun@gmail.com>
2016-02-22 03:28:34 +0000

    same example with promises

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+'use strict';
+
 const Node = require('./lib/node'),
 			_ = require('lodash');
 
 const numNodes = 2;
 const nodeOpts = _.range(numNodes).map(e => { return { id: e, addr: '127.0.0.1', port: 3100 + e } });
-const nodes = nodeOpts.map(opt => Node(opt.id, opt.addr, opt.port));
-setTimeout(() => {
-	nodes[0].ping(nodes[1], (err, result) => {
-		console.log('err', err);
-		console.log('results', result);
-	})
-}, 1000);
+const nodesP = nodeOpts.map(opt => Node(opt.id, opt.addr, opt.port));
+Promise.all(nodesP).then(nodes => {
+  nodes[0].ping(nodes[1]);
+});

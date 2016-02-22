c0eab75526e235760a8469b3842639f9bcbd8376
raelcun <dt.raelcun@gmail.com>
2016-02-22 05:11:44 +0000

    ping response working

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ const numNodes = 2;
 const nodeOpts = _.range(numNodes).map(e => { return { id: e, addr: '127.0.0.1', port: 3100 + e } });
 const nodesP = nodeOpts.map(opt => Node(opt.id, opt.addr, opt.port));
 Promise.all(nodesP).then(nodes => {
-  nodes[0].ping(nodes[1]);
+  nodes[0].ping(nodes[1]).then(result => {
+    console.log('ping result', result);
+  });
 });

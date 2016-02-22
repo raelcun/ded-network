9ad00dd958cb912cc0d221594d58383fcbc03098
Dan Taylor <dt.raelcun@gmail.com>
2016-02-21 21:35:38 -0500

    working hardcoded ping

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-module.exports = {
-  Node: require('./lib/node'),
-  Router: require('./lib/router'),
-  Message: require('./lib/message')
-}
+const Node = require('./lib/node'),
+			_ = require('lodash');
+
+const numNodes = 2;
+const nodeOpts = _.range(numNodes).map(e => { return { id: e, addr: '127.0.0.1', port: 3100 + e } });
+const nodes = nodeOpts.map(opt => Node(opt.id, opt.addr, opt.port));
+setTimeout(() => {
+	nodes[0].ping(nodes[1], (err, result) => {
+		console.log('err', err);
+		console.log('results', result);
+	})
+}, 1000);

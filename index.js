63d1a0fab7815c12ebc3ac2d2c1b9d63dc0aa911
raelcun <dt.raelcun@gmail.com>
2016-02-23 03:22:03 +0000

    mocha tests work now. Yeah!

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const Node = require('./lib/node'),
 			_ = require('lodash');
 
 const numNodes = 2;
-const nodeOpts = _.range(numNodes).map(e => { return { id: e, addr: '127.0.0.1', port: 3100 + e } });
+const nodeOpts = _.range(numNodes).map(e => { return { addr: '127.0.0.1', port: 3100 + e } });
 const nodesP = nodeOpts.map(opt => Node(opt.id, opt.addr, opt.port));
 Promise.all(nodesP).then(nodes => {
-  nodes[0].ping(nodes[1]).then(result => {
-    console.log('ping result', result);
-  });
+  nodes[0].connect(nodes[1]);
 });
---
+++ test/integration.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const expect = require('chai').expect,
+      Node = require('../lib/node'),
+      _ = require('lodash');
+     
+const numNodes = 5;
+const nodeOpts = _.range(numNodes).map(e => { return { addr: '127.0.0.1', port: 3100 + e }});
+
+const internals = {};
+internals.nodes = [];
+
+describe('integration', () => {
+  beforeEach(done => {
+    Promise.all(nodeOpts.map(opt => Node(opt.id, opt.addr, opt.port))).then(nodes => {
+      internals.nodes = nodes;
+      done();
+    });
+  });
+  
+  describe('Node', () => {
+    it('#ping', (done) => {
+      internals.nodes[0].ping(internals.nodes[1]).then(result => {
+        expect(true).to.equal(true);
+        done();
+      });
+    });
+  });
+});

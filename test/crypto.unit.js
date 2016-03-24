a7a0b86b9bb38a997476fe77409e940d1485f9f1
marti2dv <marti2dv@gmail.com>
2016-03-23 23:49:52 -0400

    cleaned up crypto module of funcitons we no longer use. Fixed tests to match

--- test/crypto.unit.js
+++ test/crypto.unit.js
@@ -32,7 +32,7 @@ describe('Crypto', () => {
 			const data = 'super secret data';
 			const kp = crypto.generateKeyPair();
 			const encrypted = crypto.encrypt(kp.public, data);
-			const decrypted = kp.decrypt(encrypted);
+			const decrypted = crypto.decrypt(kp.private, encrypted);
 			expect(decrypted).to.equal(data);
 		})
 
@@ -41,23 +41,17 @@ describe('Crypto', () => {
 	describe('#sign', () => {
 
 		it('should create a digital signature string', () => {
-			const signature = crypto.generateKeyPair().sign('super secret data');
+      const kp = crypto.generateKeyPair();
+			const signature = crypto.sign(kp.private, 'super secret data');
 			expect(typeof signature).to.equal('string');
 		})
 
 		it('should return a string different from the input', () => {
 			const data = 'super secret data';
-			const signature = crypto.generateKeyPair().sign(data);
+      const kp = crypto.generateKeyPair();
+			const signature = crypto.sign(kp.private, data);
+			expect(typeof signature).to.equal('string');
 			expect(signature).to.not.equal(data);
-		})
-
-		it('should always create a signature of constant length', () => {
-			const data1 = 'super secret data';
-			const data2 = 'secret data with a different length';
-			const sig_size = constants.SIGNATURE_SIZE;
-			const signature1 = crypto.generateKeyPair().sign(data1);
-			const signature2 = crypto.generateKeyPair().sign(data2);
-			expect(sig_size).all.to.equal(signature1.length, signature2.length);
 		})
 
 	})
@@ -67,38 +61,12 @@ describe('Crypto', () => {
 		it('should verify signed value', () => {
 			const data = 'super secret data';
 			const kp = crypto.generateKeyPair();
-			const signature = kp.sign(data);
+			const signature = crypto.sign(kp.private, data);
 			const bool = crypto.verify(kp.public, data, signature);
 			expect(bool).to.be.true;
 		})
 
 	})
-
-  describe('#encryptAndSign', () => {
-
-    it('should encrypt data and add signature', () => {
-      const data = 'super secret data';
-      const source_kp = crypto.generateKeyPair();
-      const dest_kp = crypto.generateKeyPair();
-      const payload = source_kp.encryptAndSign(dest_kp.public, data);
-      expect(typeof payload).to.equal('string');
-    })
-
-  })
-
-  describe('#decryptAndVerify', () => {
-
-    it('should decrypt data and verify signature', () => {
-      const data = 'super secret data';
-      const source_kp = crypto.generateKeyPair();
-      const dest_kp = crypto.generateKeyPair();
-      const payload = source_kp.encryptAndSign(dest_kp.public, data);
-      const output = dest_kp.decryptAndVerify(source_kp.public, payload);
-      expect(output.payload).to.equal(data);
-      expect(output.verified).to.be.true;
-    })
-
-  })
 
   describe('#aesEncrypt', () => {
 

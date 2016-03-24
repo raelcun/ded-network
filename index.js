26522dd02560b2f2065b0415205d6619923bc0f0
marti2dv <marti2dv@gmail.com>
2016-03-23 21:03:26 -0400

    changed index.js file to interactivish debugging program

--- index.js
+++ index.js
@@ -7,25 +7,73 @@ const Node = require('./lib/node'),
 			_ = require('lodash'),
 			Logger = require('./lib/logger');
 
-const numNodes = 10;
-const debug = true;
-const logger = Logger({ minLevel: debug ? 0 : 4, maxLevel: 1 });
+let numNodes = 20;
+const debug = false;
+const logger = Logger({ minLevel: debug ? 0 : 3, maxLevel: 4 });
 const nodeOpts = _.range(numNodes).map(e => { return { ip: '127.0.0.1', port: 3100 + e, logger}; });
 
 const internals = {};
 
-Promise.all(nodeOpts.map(opt => Node(opt))).then(nodes => {
-	internals.nodes = nodes;
-  var BaseNode = internals.nodes[0];
-	var nodes = _.slice(internals.nodes, 1, internals.nodes.length);
+// main needed for async
+const main = async () => {
+  logger.warn('nodes initializing');
+  const nodes = await Promise.all(nodeOpts.map((opts, i) => Node((i+1).toString(), opts)));
 
-	var p = Promise.resolve();
-	nodes.forEach(node => {
-	  p = p.then(() => {
-	    return node.connect(BaseNode);
-	  });
-	});
-	p.then(() => { 
-	  console.log('test')
-	})
-});
+  logger.warn('nodes connecting');
+  for (let i = 1; i < nodes.length; i++){
+    await nodes[i].connect(nodes[0]);
+  }
+  logger.warn('finished connecting');
+
+
+  process.stdin.on('data', async text => {
+    const input = text.toString().trim().split(' ');
+
+    if (input[0] == 'close'){
+      logger.warn('nodes closing');
+      nodes.map(e => e.close());
+    }
+    if (input[0] == 'exit'){
+      process.exit();
+    }
+
+    if (input[0] == 'key'){
+      const key = await nodes[Number(input[1])-1].findPublicKeyP(input[2]);
+      console.log(key);
+    }
+
+    if (input[0] == 'allbuckets'){
+      nodes.forEach(node => {
+        logger.warn('node', node.username);
+        node.router._buckets.forEach((bucket, j) => {
+          console.log(j, bucket.filter(e => e !== undefined).map(e => e.username));
+        });
+      });
+    }
+
+    if (input[0] == 'buckets'){
+      nodes[Number(input[1])-1].router._buckets.forEach((bucket, j) => {
+        console.log(j, bucket.filter(e => e !== undefined).map(e => e.username));
+      });
+    }
+
+    if(input[0] == 'message'){
+      const message = _.slice(input, 3).join(' ') || 'hello';
+      const key = await nodes[Number(input[1])-1].findPublicKeyP(input[2]);
+      const response = await nodes[Number(input[1])-1].sendMessage(input[2], message, key);
+      console.log('response', response);
+    }
+
+    if(input == 'addNode'){
+      const id = ++numNodes;
+      console.log(id);
+      const node = await Node((id).toString(), { ip: '127.0.0.1', port: 3100 + id, logger});
+      nodes.push(node);
+      await nodes[id-1].connect(nodes[0]);
+    }
+
+  });
+
+}
+
+main();
